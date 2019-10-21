2f752ab45d03db9914fda71f1017720af6cde4e4
Jan Slifka <jan.slifka@gmail.com>
2019-10-21 16:35:24 +0200

    Update router sctructure

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import Vuelidate from 'vuelidate'
-import VueRouter from 'vue-router'
 import BootstrapVue from 'bootstrap-vue'
-
 import 'prismjs'
 import 'prismjs/components/prism-bash'
 import 'prismjs/components/prism-scss'
@@ -11,29 +9,20 @@ import 'prismjs/themes/prism.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
 import './style.css'
-
 import App from './App.vue'
 import StatusFlash from './components/StatusFlash'
-import routes from './routes'
+import router from './router'
 import './initFontAwesome'
 
 Vue.config.productionTip = false
 
 Vue.use(Vuelidate)
-Vue.use(VueRouter)
 Vue.use(BootstrapVue)
 
 
 Vue.component('fa', FontAwesomeIcon)
 Vue.component('status-flash', StatusFlash)
-
-const router = new VueRouter({
-  mode: 'history',
-  linkExactActiveClass: 'active',
-  routes
-})
 
 new Vue({
   render: h => h(App),
---
+++ src/router/index.js
@@ -0,0 +1,46 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import ApplicationCreate from '../views/ApplicationCreate'
+import ApplicationDetail from '../views/ApplicationDetail'
+import ApplicationList from '../views/ApplicationList'
+import InstanceList from '../views/InstanceList.vue'
+import InstanceDetail from '../views/InstanceDetail.vue'
+import InstanceCreate from '../views/InstanceCreate.vue'
+import Login from '../views/Login.vue'
+import NotFound from '../views/NotFound.vue'
+import ServerDetail from '../views/ServerDetail.vue'
+import ServerCreate from '../views/ServerCreate.vue'
+import ServerList from '../views/ServerList.vue'
+import UserCreate from '../views/UserCreate'
+import UserDetail from '../views/UserDetail'
+import UserList from '../views/UserList'
+
+
+Vue.use(VueRouter);
+
+const routes = [
+  { path: '/', redirect: '/instances' },
+  { path: '/applications', component: ApplicationList },
+  { path: '/applications/create', component: ApplicationCreate },
+  { path: '/applications/:id', component: ApplicationDetail },
+  { path: '/instances', component: InstanceList },
+  { path: '/instances/create', component: InstanceCreate },
+  { path: '/instances/:id', component: InstanceDetail },
+  { path: '/login', component: Login, name: 'login' },
+  { path: '/servers', component: ServerList },
+  { path: '/servers/create', component: ServerCreate },
+  { path: '/servers/:id', component: ServerDetail },
+  { path: '/users', component: UserList },
+  { path: '/users/create', component: UserCreate },
+  { path: '/users/:id', component: UserDetail },
+  { path: '*', component: NotFound }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  linkExactActiveClass: 'active',
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
